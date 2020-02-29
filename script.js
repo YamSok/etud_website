f4ef98241504c92e0bebe23ed0c7afb0340354fc
yamsok <elouazza@etud.insa-toulouse.fr>
2020-02-29 22:44:17 +0100

    btn info

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 window.onload = function() {
-    document.getElementById("btn").onclick = function fun() {
+    btn = document.querySelectorAll('#btn');
+    
+    
+    btn[0].onclick = function fun() { 
         p = document.querySelectorAll('.back');
        for (i=0; i<3; i++) {
            p[i].classList.toggle('animback');
                            }
     }
+        btn[1].onclick = function fun() { 
+        p = document.querySelectorAll('.back');
+       for (i=3; i<6; i++) {
+           p[i].classList.toggle('animback');
+                           }
+    }
+            btn[2].onclick = function fun() { 
+        p = document.querySelectorAll('.back');
+       for (i=6; i<9; i++) {
+           p[i].classList.toggle('animback');
+                           }
+    }
+
 }
